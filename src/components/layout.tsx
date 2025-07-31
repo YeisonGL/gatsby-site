8302e7e45e5a4b1683e4324c2ea6f89d981f7ef5
Yeison Gonzalez <ydgonzalez@ccxc.us>
2025-07-31 15:42:07 -0500

    [ADD] finish part 2

---
+++ src/components/layout.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import styled from "styled-components"
+
+const Container = styled.div`
+    margin: auto;
+    max-width: 31.25rem;
+    font-family: sans-serif;
+`;
+
+const Heading = styled.h1`
+    color: rebeccapurple;
+`;
+
+const NavLinks = styled.ul`
+    display: flex;
+    list-style: none;
+    padding-left: 0;
+`;
+
+const NavLinkItem = styled.li`
+    padding-right: 2rem;
+`;
+
+const NavLinkText = styled(Link)`
+    color: black;
+`;
+
+const Layout: React.FC<{ pageTitle: string, children: React.ReactNode }> = ({ pageTitle, children }) => {
+  return (
+    <Container>
+      <nav>
+        <NavLinks>
+          <NavLinkItem><NavLinkText to="/">Home</NavLinkText></NavLinkItem>
+          <NavLinkItem><NavLinkText to="/about">About</NavLinkText></NavLinkItem>
+        </NavLinks>
+      </nav>
+      <main>
+        <Heading>{pageTitle}</Heading>
+        {children}
+      </main>
+    </Container>
+  )
+}
+
+export default Layout
